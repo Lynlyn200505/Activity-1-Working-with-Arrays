9e0fe16a13b5d043fc157fa315d16a23fc814ed9
Lynlyn200505 <ailynlucina1727@gmail.com>
2024-10-19 13:07:19 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,8 @@
+// Favorite Singers
+let favSingers = ["Ed Sheeran", "Bruno Mars", "Arthur Nery"];
+console.log("My first favorite singer is", favSingers[0]);
+
+// Favorite Numbers
+let favNumbers = ["17", "22", "03", "19"];
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr);
